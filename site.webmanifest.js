56b9396beb92aae5f3c68ebd1f510d6898cba148
cryptococktailclub <info@cryptococktailclub.com>
2025-12-11 10:18:16 +0200

    Add files via upload

---
+++ site.webmanifest.js
@@ -0,0 +1,22 @@
+{
+  "name": "Crypto Cocktail Club – Virtual Bar Experience",
+  "short_name": "CCC Bar",
+  "start_url": "/",
+  "scope": "/",
+  "display": "standalone",
+  "orientation": "portrait-primary",
+  "background_color": "#05010f",
+  "theme_color": "#05010f",
+  "icons": [
+    {
+      "src": "/android-chrome-192.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    },
+    {
+      "src": "/android-chrome-512.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
