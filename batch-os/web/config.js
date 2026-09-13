72dfe06254a815cfd1aa4bd5532f085fa298ec59
mnewton <mnewton4387@gmail.com>
2026-09-13 18:23:18 +0300

    Load searchable ingredient comboboxes

--- batch-os/web/config.js
+++ batch-os/web/config.js
@@ -12,7 +12,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
   if (!document.querySelector('script[data-batch-recipe-suggestions]')) {
     const script = document.createElement('script');
-    script.src = 'recipe-suggestions-v12.js?v=12';
+    script.src = 'recipe-suggestions-v13.js?v=13';
     script.dataset.batchRecipeSuggestions = 'true';
     document.body.appendChild(script);
   }
